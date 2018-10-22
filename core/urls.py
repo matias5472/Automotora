e772b6e2e5b1dfb147aa8625602322a0b8eba3cd
Matias <matias.gamalier@gmail.com>
2018-10-22 10:30:38 -0300

    Proyecto para el miercoles

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import home, galeria, agregar_auto,listar_autos,eliminar_auto
+from .views import home, galeria, formulario, agregar_mascota
 
 urlpatterns = [
-    path('', home, name="home"),
-    path('galeria/', galeria, name="galeria"),
-    path('agregar-auto/', agregar_auto, name="agregar_auto"),
-    path('listar-autos/', listar_autos, name="listar_autos"),
-    path('eliminar-auto/<id>/',eliminar_auto, name="eliminar_auto"),
+    path('', home, name='home'),
+    path('galeria/', galeria, name='galeria'),
+    path('registro/', formulario, name='formulario'),
+    path('Registro-de-Mascota/', agregar_mascota, name="agregar_mascota"),
 ]
